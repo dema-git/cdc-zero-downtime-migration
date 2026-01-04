3d92251eeb4d7311a49c65bb9ad6ca2f25b503ab
dema-git <git.dema@gmail.com>
2026-01-04 19:36:20 +0100

    add: .sh scripts to check if DB already exist Ref #5

---
+++ app/init_db_legacy.py
@@ -0,0 +1,23 @@
+import os
+from urllib.parse import quote_plus
+from sqlalchemy import create_engine
+from models import Base
+
+print("Checking tables in database...")
+
+
+POSTGRES_USER = os.getenv("POSTGRES_LEGACY_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_LEGACY_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "legacy")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db_legacy")
+POSTGRES_PORT = os.getenv("POSTGRES_LEGACY_PORT", "5432")
+
+POSTGRES_PASSWORD = quote_plus(POSTGRES_PASSWORD)
+
+DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+Base.metadata.create_all(engine)
+
+print(f"✅ Tables in database '{POSTGRES_DB}' are created (if they didn't exist)")
