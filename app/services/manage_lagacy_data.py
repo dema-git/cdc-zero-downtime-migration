351a5f12ab117278c7a65e2629acceb803539904
dema-git <git.dema@gmail.com>
2026-01-17 15:18:29 +0100

    create: kafka consumer to send clean customers to kafka topic Ref #22

--- app/services/manage_lagacy_data.py
+++ app/services/manage_lagacy_data.py
@@ -1,4 +1,12 @@
 from app.models import CDCEvent
+from confluent_kafka import Producer
+import json
+
+
+conf = {'bootstrap.servers': 'kafka:9092'}
+producer = Producer(conf)
+topic = "cleared_customers"
+
 
 def before_data(data: dict):
     table = data.get("table")
@@ -18,4 +26,6 @@ def manage_legacy_data_main(data_batch: list[CDCEvent]):
     for event in data_batch:
         if event.table_name == 'legacy_customers':
             data_to_send = event.with_split_name()
+            producer.produce(topic, json.dumps(data_to_send).encode("utf-8"))
 
+    producer.flush()
