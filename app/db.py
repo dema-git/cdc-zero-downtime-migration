23e72cd5d1a951f1f36d078ebbc06c6138561108
dema-git <git.dema@gmail.com>
2026-01-23 20:57:44 +0100

    add: comments to db.py file Ref #36

--- app/db.py
+++ app/db.py
@@ -1,8 +1,14 @@
+#############################################################
+# db.py
+#
+# This module initializes the SQLAlchemy engine and session
+# for the legacy PostgreSQL database.
+#############################################################
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 DATABASE_URL = "postgresql+psycopg2://postgres:pass12345%401@db_legacy:5432/legacy"
-
 
 engine = create_engine(
     DATABASE_URL,
