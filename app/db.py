51aa7296291c1fe6a7a5588eb77ce5d1c3436352
dema-git <git.dema@gmail.com>
2026-01-24 19:09:14 +0100

    add: sessionmaker for clean db to db.py Ref #42

--- app/db.py
+++ app/db.py
@@ -1,22 +1,43 @@
 #############################################################
 # db.py
 #
-# This module initializes the SQLAlchemy engine and session
-# for the legacy PostgreSQL database.
+# This module initializes the SQLAlchemy engines and sessions
+# for the legacy and clean PostgreSQL databases.
 #############################################################
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
+##############
+# LEGACY DB
+##############
+
 DATABASE_URL = "postgresql+psycopg2://postgres:pass12345%401@db_legacy:5432/legacy"
 
-engine = create_engine(
+legacy_engine = create_engine(
     DATABASE_URL,
     echo=False,
     pool_pre_ping=True,
 )
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+LegacyDBSession = sessionmaker(bind=legacy_engine, autoflush=False, autocommit=False)
+
+
+##############
+# CLEAN DB
+##############
+
+CLEAN_DATABASE_URL = "postgresql+psycopg2://admin2:pass12345%402@db_clean:5432/clean"
+
+clean_engine = create_engine(
+    CLEAN_DATABASE_URL,
+    echo=False,
+    pool_pre_ping=True,
+)
+
+CleanDBSession = sessionmaker(bind=clean_engine,autoflush=False, autocommit=False)
+
+
 
 
 class Base(DeclarativeBase):
